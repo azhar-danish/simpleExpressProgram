d84a86e8054af3576f3297d672580a3ce6ef41d4
Azhar Danish <azhardanish@Azhars-MacBook-Air.local>
2026-03-01 02:32:25 +0530

    update the code

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World! hello azhar,This is deployment.Hello AGL')
 })
+app.get('/home', (req, res) => {
+  res.send('Welcom to home')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
