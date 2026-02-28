95aa164f8f45d8cea3be7c2a5c3729b78c46bd29
Azhar Danish <azhardanish@Azhars-MacBook-Air.local>
2026-03-01 02:19:54 +0530

    update the code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! hello azhar,This is deployment')
+  res.send('Hello World! hello azhar,This is deployment.Hello AGL')
 })
 
 app.listen(port, () => {
